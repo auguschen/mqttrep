bbc9b618bd3b65a42772d37cd6f616a3ac309a72
augus <tianhao.chen@gmail.com>
2015-08-19 14:41:14 +0800

    sadas

--- relay/mqtt/mqttrelay.py
+++ relay/mqtt/mqttrelay.py
@@ -6,6 +6,17 @@ Created on Aug 19, 2015
 import MySQLdb
 import paho.mqtt.client as mqtt
 
+mqttserveraddr = "iot.darktech.org"
+mqttserverport = 1883
+
+#mqttmontopic = "$INPUT"
+mqttmontopic = "tmp"
+
+mysqlhost = "localhost"
+mysqlport = 3306
+mysqluser = "mqtt"
+mysqlpass = "p2ssw0rd"
+mysqlprefix = "mqtt_"
 def initdb():
     pass
 
@@ -14,10 +25,11 @@ def saveTodb(db):
 
 def on_connect(client,userdata,flags,rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("tmp")
+    client.subscribe(mqttmontopic)
 
 def on_message(client,userdata,message):
     print(message.topic+" "+str(message.payload))
+    saveTodb()
 
 
 if __name__ == '__main__':
@@ -25,6 +37,10 @@ if __name__ == '__main__':
     mqttclient = mqtt.Client()
     mqttclient.on_connect = on_connect
     mqttclient.on_message = on_message
-    mqttclient.connect("iot.darktech.org",1883,60)
-    mqttclient.loop_forever()
+    try:
+        mqttclient.connect(mqttserveraddr,mqttserverport,60)
+        mqttclient.loop_forever()
+    except KeyboardInterrupt:
+        mqttclient.disconnect()
+        
     
