85d2bad77d15a9487b26de8586fc32c0e6fff179
augus <tianhao.chen@gmail.com>
2015-08-19 14:09:16 +0800

    sss

--- relay/mqtt/mqttrelay.py
+++ relay/mqtt/mqttrelay.py
@@ -6,5 +6,18 @@ Created on Aug 19, 2015
 import MySQLdb
 import paho_mqtt_client as mqtt
 
+def initdb():
+    pass
+
+def saveTodb(db):
+    pass
+
+def on_connect(client,userdata,flags,rc):
+    pass
+
+def on_message(client,userdata,message):
+    pass
+
+
 if __name__ == '__main__':
     print "Hello World"
